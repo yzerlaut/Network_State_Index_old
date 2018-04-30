ebf4df7e0e15191bc4a726ca4396a74158205fe0
yzerlaut <yann.zerlaut@gmail.com>
2018-04-30 18:48:42 +0200

    done with the overall gui draft

---
+++ src/functions.py
@@ -0,0 +1,152 @@
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+import numpy as np
+from scipy import signal
+from scipy.ndimage.filters import gaussian_filter1d
+
+##############################################
+########### Wavelet Transform ################
+##############################################
+
+def my_cwt(data, frequencies, dt, w0=6.):
+    """
+    wavelet transform with normalization to catch the amplitude of a sinusoid
+    """
+    output = np.zeros([len(frequencies), len(data)], dtype=np.complex)
+
+    for ind, freq in enumerate(frequencies):
+        wavelet_data = np.conj(get_Morlet_of_right_size(freq, dt, w0=w0))
+        sliding_mean = signal.convolve(data,
+                                       np.ones(len(wavelet_data))/len(wavelet_data),
+                                       mode='same')
+        # the final convolution
+        wavelet_data_norm = norm_constant_th(freq, dt, w0=w0)
+        output[ind, :] = signal.convolve(data-sliding_mean+0.*1j,
+                                         wavelet_data,
+                                         mode='same')/wavelet_data_norm
+    return output
+
+### MORLET WAVELET, definition, properties and normalization
+def Morlet_Wavelet(t, f, w0=6.):
+    x = 2.*np.pi*f*t
+    output = np.exp(1j * x)
+    output *= np.exp(-0.5 * ((x/w0) ** 2)) # (Normalization comes later)
+    return output
+
+def Morlet_Wavelet_Decay(f, w0=6.):
+    return 2 ** .5 * (w0/(np.pi*f))
+
+def from_fourier_to_morlet(freq):
+    x = np.linspace(0.1/freq, 2.*freq, 1e3)
+    return x[np.argmin((x-freq*(1-np.exp(-freq*x)))**2)]
+    
+def get_Morlet_of_right_size(f, dt, w0=6., with_t=False):
+    Tmax = Morlet_Wavelet_Decay(f, w0=w0)
+    t = np.arange(-int(Tmax/dt), int(Tmax/dt)+1)*dt
+    if with_t:
+        return t, Morlet_Wavelet(t, f, w0=w0)
+    else:
+        return Morlet_Wavelet(t, f, w0=w0)
+
+def norm_constant_th(freq, dt, w0=6.):
+    # from theoretical calculus:
+    n = (w0/2./np.sqrt(2.*np.pi)/freq)*(1.+np.exp(-w0**2/2))
+    return n/dt
+
+##################################################
+########### Processing of the LFP ################
+##################################################
+
+def gaussian_smoothing(Signal, idt_sbsmpl=10.):
+    """Gaussian smoothing of the data"""
+    return gaussian_filter1d(Signal, idt_sbsmpl)
+
+def preprocess_LFP(data,
+                   freqs = np.linspace(50, 300, 5), 
+                   new_dt = 5e-3,
+                   smoothing=42e-3,
+                   percentile_for_p0=0.01,                   
+                   pLFP_unit='$\mu$V'):
+    """
+    performs continuous wavelet transform and smooth the time-varying high-gamma freq power
+    """
+
+    # performing wavelet transform
+    data['W'] = my_cwt(data['Extra'].flatten(), freqs, data['dt']) 
+    data['pLFP_freqs'] = freqs # keeping track of the frequency used
+
+    # taking the mean power over the frequency content considered
+    W2 = np.abs(data['W']).mean(axis=0)
+    isubsmpl = int(new_dt/data['dt'])
+    
+    # then smoothing and subsampling
+    data['pLFP'] = gaussian_smoothing(\
+                                      np.reshape(W2[:int(len(W2)/isubsmpl)*isubsmpl],
+                                                 (int(len(W2)/isubsmpl),isubsmpl)).mean(axis=1),
+                                      int(smoothing/new_dt)).flatten()
+    data['new_dt'] = new_dt
+    data['new_t'] = np.arange(len(data['pLFP']))*data['new_dt']
+    # find p0
+    data['p0'] = np.percentile(data['pLFP'], percentile_for_p0)
+
+def heaviside(x):
+    return (np.sign(x)+1)/2
+
+def Network_State_Index(data,
+                        p0=0.,
+                        alpha=2.):
+    
+    NSI=np.zeros(len(data['pLFP']))
+    # where rhythmicity is matched
+    X = (p0+alpha*data['max_low_freqs_power'])-data['sliding_mean']
+    NSI = -2*data['max_low_freqs_power']*heaviside(X)+heaviside(-X)*(data['sliding_mean']-p0)
+    return NSI
+
+def Validate_Network_States(data, 
+                            Tstate=200e-3,
+                            Var_criteria=2):
+    
+    # validate states:
+    iTstate = int(Tstate/data['new_dt'])
+    # validate the transitions
+    data['NSI_validated'] = np.zeros(len(data['pLFP']), dtype=bool)
+    data['NSI_unvalidated'] = np.zeros(len(data['pLFP']), dtype=bool)
+    for i in np.arange(len(data['pLFP']))[::iTstate][1:-1]:
+        if np.array(np.abs(data['NSI'][i-iTstate:i+iTstate]-data['NSI'][i])<=Var_criteria).all():
+            data['NSI_validated'][i]=True
+        else:
+            data['NSI__unvalidated'][i]=True
+
+    data['t_validated'] = data['new_t'][data['NSI_validated']]
+    data['i_validated'] = np.arange(len(data['pLFP']))[data['NSI_validated']]
+
+    
+def compute_Network_State_Index(data,
+                                key='pLFP',
+                                freqs = np.linspace(2,10,20),
+                                Tstate=200e-3,
+                                Var_criteria=2,
+                                alpha=2.85,
+                                T_sliding_mean=0.5,
+                                already_low_freqs_and_mean=False):
+    
+    if not already_low_freqs_and_mean:
+        # sliding mean
+        data['sliding_mean'] = gaussian_smoothing(data[key], int(T_sliding_mean/data['new_dt']))
+
+        # low frequency power
+        data['low_freqs'] = freqs # storing the used-freq
+        data['W_low_freqs'] = my_cwt(data[key].flatten(), freqs, data['new_dt']) # wavelet transform
+        data['max_low_freqs_power'] = np.max(np.abs(data['W_low_freqs']), axis=0) # max of freq.
+    
+    data['NSI']= Network_State_Index(data,
+                                     p0 = data['p0'],
+                                     alpha=alpha)
+    
+    Validate_Network_States(data,
+                            Tstate=Tstate,
+                            Var_criteria=Var_criteria)
+    
+    
+if __name__=='__main__':
+    print(gaussian_smoothing(np.linspace(0,100,50), 1.3)) # translating to integer values... so keep in mind
--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,9 @@
-import sys, os
+import sys, os, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from src.functions import * # all functions required to make the analysis
+
+# then modules for GUI
 import matplotlib
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -6,65 +11,63 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pylab as plt
 plt.style.use('ggplot')
 from PyQt5 import QtGui, QtWidgets, QtCore
-import numpy as np
-# from analyze_datafile import plot_data, load_data_and_initialize, save_as_npz
-# from IO.files_manip import get_files_with_given_exts, rename_files_for_easy_sorting
-# from graphs.my_graph import make_multipanel_fig
-# from automated_analysis import analysis_window
-# from IO.load_data import load_file, get_metadata
+
+Blue, Orange, Green, Red, Purple, Brown, Pink, Grey,\
+    Kaki, Cyan = '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',\
+    '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
 
 FONTSIZE= 5
 matplotlib.rcParams.update({'axes.labelsize': FONTSIZE,
                             'axes.titlesize': FONTSIZE,
                             'figure.titlesize': FONTSIZE,
                             'font.size': FONTSIZE,
-                            'legend.fontsize': FONTSIZE,
                             'xtick.labelsize': FONTSIZE,
-                            'ytick.labelsize': FONTSIZE,
-                            'figure.facecolor': 'none',
-                            'legend.facecolor': 'none',
-                            'axes.facecolor': 'none',
-                            'savefig.facecolor': 'none'})
+                            'ytick.labelsize': FONTSIZE})
 
-def create_plot_window(parent, hspace=0.01, left=0.05, right=0.99):
+def create_plot_window(parent, hspace=0.1, left=0.08, right=0.99, bottom=0.2, figsize=(8,3)):
 
-    width, height = parent.screensize[0]/1.1, parent.screensize[1]/2.
+    width, height = parent.screensize[0]/1.1, parent.screensize[1]/1.5
     
-    fig_large_view, AX_large_view = plt.subplots(3, 1, figsize=(7,2))
-    plt.subplots_adjust(hspace=hspace, left=left, right=right)
-    for i in range(3):
-        if i<2:
-            AX_large_view[i].set_xticklabels([])
-
-    fig_zoom, AX_zoom  = plt.subplots(3, 1, figsize=(7,3))
-    plt.subplots_adjust(hspace=hspace, left=left, right=right)
-    for i in range(3):
-        if i<2:
-            AX_zoom[i].set_xticklabels([])
+    fig_large_view, AX_large_view = plt.subplots(3, 1, figsize=(figsize[0], figsize[1]/1.5))
+    plt.subplots_adjust(hspace=hspace, left=left, right=right, bottom=1.5*bottom)
+    AX_large_view[0].set_xticklabels([])
+    AX_large_view[1].set_xticklabels([])
+    # AX_large_view[2].set_xlabel('time (s)')
+        
+    fig_zoom, AX_zoom  = plt.subplots(3, 1, figsize=(figsize[0], figsize[1]))
+    plt.subplots_adjust(hspace=hspace, left=left, right=right, bottom=bottom)
+    AX_zoom[0].set_xticklabels([])
+    AX_zoom[1].set_xticklabels([])
+    AX_zoom[2].set_xlabel('time (s)')
             
-    # # get all figures with their size !
+    for ax1, ax2, label in zip(AX_large_view, AX_zoom,
+                               ['$V_{ext}$ ($\mu$V)', 'pLFP ($\mu$V)', 'NSI ($\mu$V)']):
+        ax1.set_ylabel(label+'              ', rotation=0)
+        ax2.set_ylabel(label)
+        ax1.set_yticks([])
     
     # Window size choosen appropriately
     window = QtWidgets.QDialog()
-    # window.setGeometry(100, 150, width, height)
     window.setGeometry(10, 150, width, height)
-    
+
     # this is the Canvas Widget that displays the `figure`
     # it takes the `figure` instance as a parameter to __init__
+    
     canvas_large_view = FigureCanvas(fig_large_view)
     canvas_zoom = FigureCanvas(fig_zoom)
-    layout = QtWidgets.QGridLayout(window)
 
+    toolbar_zoom = NavigationToolbar(canvas_zoom, window)
+    toolbar_zoom.hide()
+    
+    layout = QtWidgets.QGridLayout(window)
     layout.addWidget(canvas_large_view)
     layout.addWidget(canvas_zoom)
+    layout.addWidget(toolbar_zoom)
         
     window.setLayout(layout)
             
-    return window, AX_large_view, AX_zoom
+    return window, AX_large_view, AX_zoom, canvas_large_view, canvas_zoom
 
-# def get_list_of_files(cdir="/tmp"):
-#     return get_files_with_given_exts(cdir)
-        
 class Window(QtWidgets.QMainWindow):
     
     def __init__(self, app, parent=None, DATA_LIST=None, KEYS=None):
@@ -72,10 +75,11 @@ class Window(QtWidgets.QMainWindow):
         super(Window, self).__init__(parent)
         
         # buttons and functions
-        LABELS = ["q) Quit", "o) Open File"]
-        FUNCTIONS = [self.close_app, self.file_open]
-        button_length = 113.
-        self.setWindowIcon(QtGui.QIcon('pics/logo.png'))
+        LABELS = ["q) Quit", "o) Open File", "r) Run analysis", "s) Save as .bin",
+                  "Save as txt", "z) Zoom1", "Zoom2"]
+        FUNCTIONS = [self.close_app, self.file_open, self.analyze, self.close_app,\
+                     self.close_app, self.close_app, self.close_app]
+        button_length = 130
         self.setWindowTitle('Computing the Network State Index')
         self.setGeometry(50, 50, button_length*(1+len(LABELS)), 60)
         
@@ -96,90 +100,138 @@ class Window(QtWidgets.QMainWindow):
             action.setShortcut(label.split(')')[0])
             action.triggered.connect(func)
             self.fileMenu.addAction(action)
-
         
-        self.i_plot, self.analysis_flag = 0, False
-        self.FolderAnalysisMenu =  None
-        self.FIG_LIST, self.args, self.window2, self.window3, self.params = [], {}, None, None, {}
-        self.analysis_flag = False
-        self.data = None # DICTIONARY STORING THE DATA !
-        try:
-            self.filename,self.folder,btn_state = np.load('program_data/last_datafile.npy')
-            if btn_state=='False': self.btn.setChecked(False)
-            self.FILE_LIST = get_list_of_files(self.folder)
-            self.i_plot = np.argwhere(self.FILE_LIST==self.filename)[0][0]
-            load_data_and_initialize(self)
-            self.update_plot()    
-        except (FileNotFoundError, ValueError, IndexError, TypeError):
-            self.filename, self.folder = '', ''
-            self.statusBar().showMessage('Provide a datafile of a folder for analysis ')
-            # self.folder = '/tmp/' # TO be Changed for Cross-Platform implementation !!
-            # self.FILE_LIST = get_list_of_files(self.folder)
-            # self.filename = self.FILE_LIST[self.i_plot]
-            # self.update_plot()
-            
-        self.window, self.AX_large_view, self.AX_zoom = create_plot_window(self)
+        # self.statusBar().showMessage('Provide a datafile for the analysis... (with "Open File" or the keyboard shortcut "o")')
+        
+        self.window,\
+            self.AX_large_view, self.AX_zoom,\
+            self.canvas_large_view, self.canvas_zoom = create_plot_window(self)
         self.window.show()    
         self.show()
 
-    # def set_analysis_folder(self):
-    #     if self.btn.isChecked():
-    #         self.analysis_folder = os.path.join(os.path.expanduser("~"),'Desktop')
-    #         try: self.FolderAnalysisMenu.hide()
-    #         except AttributeError: pass
-    #     else:
-    #         self.analysis_folder = os.path.join(self.folder,'analysis')
-    #         self.FolderAnalysisMenu =  analysis_window.FolderAnalysisMenu(self)
-    #     if not os.path.exists(self.analysis_folder):
-    #         os.makedirs(self.analysis_folder)
-    #     print('analysis folder for now: ', self.analysis_folder)
-            
-    # def analyze(self):
-    #     self.analysis_flag = True
-    #     self.statusBar().showMessage('Analyzing data [...]')
-    #     self.update_plot()    
-    #     return 0
-    
-    # def update_plot(self):
-    #     try:
-    #         self.FIG_LIST = plot_data(self)
-    #     except ValueError:
-    #         self.statusBar().showMessage('PROBLEM with datafile : '+\
-    #                                      self.filename+', Check It Manually !!')
-    #     self.window, self.window3 = create_window(self, self.FIG_LIST,
-    #                                               with_toolbar=self.analysis_flag)
-    #     self.window.show()
-    #     if self.window3 is not None:
-    #         self.window3.show()
-    #     self.statusBar().showMessage('DATA file : '+self.filename)
-    #     self.activateWindow()
-        
-    # def update_params_and_windows(self):
-    #     load_data_and_initialize(self)
-    #     self.update_plot()
-        
-    def close_app(self):
-        # if self.filename!='':
-        #     np.save('program_data/last_datafile.npy', [self.filename, self.folder, self.btn.isChecked()])
-        sys.exit()
+        # data initializaton
+        self.data = {'Extra':[], 'pLFP':[], 'NSI_validated':[], 't_validated':[], 'NSI':[], 'dt':[]}
 
+        # program defaults
+        try:
+            self.params = dict(np.load('data/last_params.npz'))
+        except (FileNotFoundError, ValueError, IndexError, TypeError):
+            self.params = {'xlim':[0,50]}
+        
+        self.filename, self.folder = 'data/sample_data.npz', 'data/'
+        self.load_data(self.filename)
+        self.large_scale_plot()
+        self.zoom_plot()
+        
+    def load_data(self, filename):
+        self.statusBar().showMessage('loading data [...]')
+        loaded_data = dict(np.load(filename))
+        for key in loaded_data:
+            self.data[key] = loaded_data[key]
+        self.dt = self.data['dt']
+        self.nsamples = len(self.data['Extra'])
+        self.statusBar().showMessage('Data loaded, now "Run Analysis"')
+        
+    def large_scale_plot(self, Nplot=2000):
+        
+        # large scale version
+        lfp_to_plot = self.data['Extra'][::int(self.nsamples/Nplot)]
+        self.AX_large_view[0].plot(np.arange(len(lfp_to_plot))*self.data['dt']*int(self.nsamples/Nplot),
+                                   lfp_to_plot, lw=0.5, color=Grey)
+        for ax in self.AX_large_view:
+            ax.set_xlim([0, self.dt*self.nsamples])
+        self.ylim0 = self.AX_large_view[0].get_ylim()
+        self.ylim1 = self.AX_large_view[1].get_ylim()
+        self.ylim2 = self.AX_large_view[2].get_ylim()
+
+    def large_scale_plot_NSI(self, Nplot=2000):
+        
+        # large scale version
+        Nsamples = len(self.data['pLFP'])
+        plfp_to_plot = self.data['pLFP'][::int(Nsamples/Nplot)]
+        self.AX_large_view[1].plot(np.arange(len(plfp_to_plot))*self.data['new_dt']*int(Nsamples/Nplot),
+                                   plfp_to_plot, lw=0.5, color=Brown)
+        cond1 = self.data['NSI_validated'] & (self.data['NSI']>0)
+        self.AX_large_view[2].plot(self.data['new_t'][cond1], self.data['NSI'][cond1], '.', ms=0.2, color=Kaki)
+        cond2 = self.data['NSI_validated'] & (self.data['NSI']<=0)
+        self.AX_large_view[2].plot(self.data['new_t'][cond2], self.data['NSI'][cond2], '.', ms=0.2, color=Purple)
+        self.ylim1 = np.min(plfp_to_plot), np.max(plfp_to_plot)
+        self.ylim2 = np.min(self.data['NSI'][self.data['NSI_validated']]), np.max(self.data['NSI'][self.data['NSI_validated']])
+        self.canvas_large_view.draw()
+        
+    def zoom_plot(self, Nplot=2000):
+
+        # cleaning up the plot before:
+        for ax in self.AX_zoom:
+            while len(ax.lines)>0:
+                del ax.lines[-1] # removing the previous plots
+        
+        i1, i2 = int(self.params['xlim'][0]/self.dt), int(self.params['xlim'][1]/self.dt)
+        lfp_to_plot = self.data['Extra'][i1:i2][::int((i2-i1)/Nplot)]
+        self.AX_zoom[0].plot(np.arange(len(lfp_to_plot))*self.data['dt']*int((i2-i1)/Nplot),
+                             lfp_to_plot, lw=0.5, color=Grey)
+        self.AX_zoom[0].set_xlim([i1*self.dt, i2*self.dt])
+
+        if len(self.data['pLFP'])>0:
+            
+            i1, i2 = int(self.params['xlim'][0]/self.data['new_dt']), int(self.params['xlim'][1]/self.data['new_dt'])
+            # plotting pLFP variations
+            plfp_to_plot = self.data['pLFP'][i1:i2][::int((i2-i1)/Nplot)]
+            self.AX_zoom[1].plot(np.arange(len(plfp_to_plot))*self.data['new_dt']*int((i2-i1)/Nplot),
+                                 plfp_to_plot, lw=1, color=Brown)
+            self.AX_zoom[1].set_xlim([i1*self.data['new_dt'], i2*self.data['new_dt']])
+            
+            # plotting NSI variations
+            nsi_to_plot = self.data['NSI'][i1:i2][::int((i2-i1)/Nplot)]
+            self.AX_zoom[2].plot(np.arange(len(plfp_to_plot))*self.data['new_dt']*int((i2-i1)/Nplot),
+                                 nsi_to_plot, lw=0.5, color='k')
+            self.AX_zoom[2].set_xlim([i1*self.data['new_dt'], i2*self.data['new_dt']])
+
+            # plotting validated NSI
+            cond = self.data['NSI_validated'] &\
+                   (self.data['new_t']>self.params['xlim'][0]) &\
+                   (self.data['new_t']<=self.params['xlim'][1])
+            self.AX_zoom[2].plot(self.data['new_t'][cond & (self.data['NSI']>0)],
+                                 self.data['NSI'][cond & (self.data['NSI']>0)], 'o',
+                                 ms=1, color=Kaki)
+            self.AX_zoom[2].plot(self.data['new_t'][cond & (self.data['NSI']<=0)],
+                                 self.data['NSI'][cond & (self.data['NSI']<=0)], 'o',
+                                 ms=1, color=Purple)
+            
+        self.canvas_zoom.draw()
+            
+        # highlighting zoom area in the large plot
+        i1, i2 = int(self.params['xlim'][0]/self.dt), int(self.params['xlim'][1]/self.dt)
+        for ax, ylim in zip(self.AX_large_view, [self.ylim0, self.ylim1, self.ylim2]):
+            while len(ax.collections)>0:
+                del ax.collections[-1] # removing the previous highlight !
+            ax.fill_between([i1*self.dt, i2*self.dt], np.ones(2)*ylim[0], np.ones(2)*ylim[1], color='r', alpha=.2, lw=0)
+            ax.set_ylim(ylim)
+        self.canvas_large_view.draw()
+        
+    def analyze(self):
+        self.statusBar().showMessage("Analyzing data [...]")
+        preprocess_LFP(self.data)
+        compute_Network_State_Index(self.data)
+        # now updating plots
+        self.large_scale_plot_NSI()
+        self.zoom_plot()
+        self.statusBar().showMessage("Data Analyzed !")
+    
     def file_open(self):
         name=QtWidgets.QFileDialog.getOpenFileName(self, 'Open File',\
                                                    self.folder)
-        self.analysis_flag = False
-        if self.FolderAnalysisMenu is not None:
-            self.FolderAnalysisMenu.close()
         try:
             self.filename = name[0]
-            load_data_and_initialize(self)
             self.folder = os.path.dirname(self.filename)
-            self.set_analysis_folder()
-            self.FILE_LIST = get_list_of_files(self.folder)
-            self.i_plot = np.argwhere(self.FILE_LIST==self.filename)[0][0]
-            self.update_params_and_windows()
         except FileNotFoundError:
             self.statusBar().showMessage('/!\ No datafile found... ')
             # pass
+
+    def close_app(self):
+        if self.params is not None:
+            np.savez('data/last_params.npz', **self.params)
+        sys.exit()
 
     def folder_open(self):
         name=QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder', self.folder)
